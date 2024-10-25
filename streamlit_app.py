b6d4f01d0127cc2e7b31f8842ea5bcfe9cd59d11
Jaemin Shin <72111485+mr-jaemin@users.noreply.github.com>
2024-10-25 16:44:36 -0400

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,32 @@
+import json
 import streamlit as st
 import numpy as np
 import math
 from collections import Counter
 
+def read_JSON_info(json_file):
+    # Load JSON data
+    json_data = json.load(json_file)
+    
+    # Default values, which will be overridden if present in JSON
+    num_dirs = json_data.get("NumberOfDiffusionDirectionGE", None)
+    num_t2 = json_data.get("NumberOfDiffusionT2GE", None)
+    phase_encoding_direction = json_data.get("PhaseEncodingDirection", None)
+
+    # Set frequency based on PhaseEncodingDirection or leave as None if not provided
+    if phase_encoding_direction in ["j", "j-"]:
+        freq = "RL"
+    elif phase_encoding_direction in ["i", "i-"]:
+        freq = "AP"
+    else:
+        freq = None  # Frequency not provided in JSON
+
+    return num_dirs, num_t2, freq
+
 def read_directions_from_file(file_content, num_dirs, num_t2):
+    num_dirs = num_dirs if num_dirs is not None else 6
+    num_t2 = num_t2 if num_t2 is not None else 1
+
     lines = file_content.splitlines()
     b_vector = np.zeros((num_dirs + num_t2, 4))
     internal_count = 0
@@ -54,76 +77,105 @@ def display_and_save_b_vector(b_vector, num_dirs, num_t2):
     ]
     return bval_output, bvec_output
 
-def main():
+def initialize_streamlit_components():
     st.title("GE tensor to bval/bvec Converter (beta)")
-    st.write("Upload a tensor file and provide the necessary arguments to convert it.")
+    st.write("Upload a JSON file to automatically extract the necessary information, or provide the values manually.")
 
-    uploaded_file = st.file_uploader("Upload Tensor File", type=["txt", "dat"])
+    # File upload options
+    uploaded_tensor_file = st.file_uploader("Upload GE tensor file (required*)", type=["txt", "dat"])
+    uploaded_json = st.file_uploader("Upload JSON file from dcm2niix (optional)", type=["json"])
+
+    st.image("GEHC_UI.png", caption="Reference Image", use_column_width=True)
+
+    # Set default values in case JSON is not uploaded
+    num_dirs, num_t2, freq = 6, 1, "RL"  # Default values
+
+    # Layout changes to put Number of Diffusion Directions and Number of T2 in one line
     col1, col2 = st.columns(2)
-    with col1:
-        num_t2 = st.number_input("Number of T2", min_value=0, step=1)
-        b_val = st.number_input("b Value", min_value=0, step=1)
-    with col2:
-        num_dirs = st.number_input("Number of Directions", min_value=1, step=1)
-       
-    freq_options = {
-        "RL (typical)": "RL",
-        "AP": "AP"
-    }
-    freq_display = st.radio("Frequency", list(freq_options.keys()))
-    freq = freq_options[freq_display]
-    verbose = st.checkbox("Verbose Output")
 
-    if st.button("Convert") or 'bval_output' in st.session_state:
-        if uploaded_file is not None:
-            file_content = uploaded_file.read().decode("utf-8")
-            b_vector = read_directions_from_file(file_content, num_dirs, num_t2)
-            convert_to_b_vector(b_vector, num_dirs, num_t2, b_val, freq)
-            bval_output = st.session_state.get('bval_output', [])
-            bvec_output = st.session_state.get('bvec_output', [])
-            st.session_state['bval_output'] = bval_output
-            st.session_state['bvec_output'] = bvec_output
-            b_vector = read_directions_from_file(file_content, num_dirs, num_t2)
-            convert_to_b_vector(b_vector, num_dirs, num_t2, b_val, freq)
-            bval_output, bvec_output = display_and_save_b_vector(b_vector, num_dirs, num_t2)
-
-            # Custom CSS to adjust the spacing
-            custom_css = """
-                <style>
-                .tight-line-spacing {
-                    line-height: 1.3;
-                    font-size: 16px;
-                    margin-bottom: 2px;
-                    font-family: monospace;
-                }
-                </style>
-            """
-            st.markdown(custom_css, unsafe_allow_html=True)
-
-            # Display the b-value summary using custom CSS and formatted spacing
-            st.write("### Summary of b-values:")
-            b_values = [int(b) for b in bval_output]
-            b_counter = Counter(b_values)
-            b_summary_html = ""
-
-            # Determine spacing to align the output
-            for b_value in sorted(b_counter):
-                b_summary_html += f"<div class='tight-line-spacing'>b={b_value: >5} x {b_counter[b_value]}</div>"
-
-            st.markdown(b_summary_html, unsafe_allow_html=True)
-
-            if verbose:
-                st.write("### bval Output:")
-                st.write(" ".join(bval_output))
-
-                st.write("### bvec Output:")
-                for line in bvec_output:
-                    st.write(line)
-
-            st.download_button("Download bval.txt", " ".join(bval_output), "bval.txt", key='download_bval')
-            st.download_button("Download bvec.txt", "\n".join(bvec_output), "bvec.txt", key='download_bvec')
+    if uploaded_json is not None:
+        num_dirs, num_t2, freq = read_JSON_info(uploaded_json)
+        with col1:
+            if num_dirs is not None:
+                st.write(f"Number of Diffusion Directions: <b>{num_dirs}</b> from JSON", unsafe_allow_html=True)
+            else:
+                num_dirs = st.number_input("Number of Diffusion Directions", min_value=1, step=1, value=6, key='num_dirs')
+        
+        with col2:
+            if num_t2 is not None:
+                st.write(f"Number of T2: <b>{num_t2}</b> from JSON", unsafe_allow_html=True)
+            else:
+                num_t2 = st.number_input("Number of T2", min_value=0, step=1, value=1, key='num_t2')
+        
+        if freq is not None:
+            st.write(f"Frequency: <b>{freq}</b> from JSON", unsafe_allow_html=True)
         else:
-            st.error("Please upload a valid tensor file.")
+            freq = st.radio("Frequency", ["RL", "AP"], key='freq')
+    else:
+        # Allow users to edit the extracted values or manually input if JSON is not uploaded
+        with col1:
+            num_dirs = st.number_input("Number of Diffusion Directions", min_value=1, step=1, value=num_dirs, key='num_dirs')
+        with col2:
+            num_t2 = st.number_input("Number of T2", min_value=0, step=1, value=num_t2, key='num_t2')
+        freq = st.radio("Frequency", ["RL", "AP"], index=["RL", "AP"].index(freq), key='freq')
+
+    b_val = st.number_input("b Value", min_value=0, step=1, value=1000, key='b_val')
+
+    return uploaded_tensor_file, num_dirs, num_t2, b_val, freq
+
+def apply_custom_css():
+    custom_css = """
+        <style>
+        .tight-line-spacing {
+            line-height: 1.3;
+            font-size: 16px;
+            margin-bottom: 2px;
+            font-family: monospace;
+        }
+        </style>
+    """
+    st.markdown(custom_css, unsafe_allow_html=True)
+
+def main():
+    uploaded_tensor_file, num_dirs, num_t2, b_val, freq = initialize_streamlit_components()
+
+    # Process tensor file if uploaded
+    if uploaded_tensor_file is not None:
+        file_content = uploaded_tensor_file.read().decode("utf-8")
+        verbose = st.checkbox("Verbose Output", key='verbose')
+
+        b_vector = read_directions_from_file(file_content, num_dirs, num_t2)
+        convert_to_b_vector(b_vector, num_dirs, num_t2, b_val, freq)
+        bval_output, bvec_output = display_and_save_b_vector(b_vector, num_dirs, num_t2)
+
+        # Apply custom CSS
+        apply_custom_css()
+
+        st.write("### Summary of b-values:")
+        b_values = [int(float(b)) for b in bval_output]
+        b_counter = Counter(b_values)
+        b_summary_html = ""
+
+        for b_value in sorted(b_counter):
+            b_summary_html += f"<div class='tight-line-spacing'>b={b_value: >5} x {b_counter[b_value]}</div>"
+
+        st.markdown(b_summary_html, unsafe_allow_html=True)
+
+        if verbose:
+            st.write("### bval Output:")
+            st.write(" ".join(bval_output))
+
+            st.write("### bvec Output:")
+            for line in bvec_output:
+                st.write(line)
+
+        file_name_prefix = uploaded_tensor_file.name.split('.')[0]
+        download_bval_name = f"{file_name_prefix}_{num_t2}t2_{num_dirs}dir_b{b_val}.bval"
+        st.download_button("Download bval file", " ".join(bval_output), download_bval_name, key='download_bval')
+        download_bvec_name = f"{file_name_prefix}_{num_t2}t2_{num_dirs}dir_b{b_val}.bvec"
+        st.download_button("Download bvec file", "\n".join(bvec_output), download_bvec_name, key='download_bvec')
+    else:
+        st.error("Please upload a valid tensor file.")
 
 if __name__ == "__main__":
     main()
